bc93585a5ca79cdb2dace6ffb16fabab52870186
gabrielleles18 <gabriel.leles18@gmail.com>
2025-10-27 16:48:48 -0300

    Commit inicial: Projeto Flutter Fokus - Aplicativo de timer Pomodoro

---
+++ lib/app/view_model/timer_view_model.dart
@@ -0,0 +1,37 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class TimerViewModel extends ChangeNotifier{
+  bool isPlaying = false;
+  Timer? timer;
+  Duration duration = Duration.zero;  
+
+  void startTimer(int initialMinutes, ValueNotifier<bool> isPaused) {
+    duration = Duration.zero;
+    isPlaying= true;
+    notifyListeners();
+
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+
+        if (isPaused.value) return;
+
+        if (duration.inMinutes < initialMinutes) {
+          duration += Duration(seconds: 1);
+          notifyListeners();
+        } else {
+          stopTime();
+        }
+    });
+  }
+
+  void stopTime() {
+    isPlaying = false;
+    notifyListeners();
+    timer?.cancel();
+  }
+}
+
+
+
